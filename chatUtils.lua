889416aa54b50c9322abb7bd8bbbe9891fcda30f
Phoenix / Hotaru <DeriousHD@gmail.com>
2025-04-13 12:36:36 +0100

    Create chatUtils.lua

---
+++ chatUtils.lua
@@ -0,0 +1,9 @@
+{
+    "name": "chatUtils,
+    "Author": "HotaruBlaze",
+    "Source": "https://github.com/HotaruBlaze/tes3mp-chatUtils",
+    "description": "A",
+    "files": [
+        "chatUtils.lua"
+    ]
+}
